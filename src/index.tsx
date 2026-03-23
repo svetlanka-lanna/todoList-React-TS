2dedc74e9f7168f27166eae0742df3d595dda08c
svetlanka-lanna <svetlanka.tch@yandex.ru>
2026-03-24 01:58:50 +0400

    Add files via upload

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './assets/scss/normalize.scss';
+import './assets/scss/style.scss';
+import { TodoListPage } from './pages/TodoListPage';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <TodoListPage />
+  </React.StrictMode>
+);
